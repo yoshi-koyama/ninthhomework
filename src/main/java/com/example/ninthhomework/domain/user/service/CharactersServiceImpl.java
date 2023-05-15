f2bc3ad4749e6eb2ec03494637ef89bcad809e9d
kamide28 <ghmaho0928@gmail.com>
2023-05-15 12:02:13 +0900

    Null判定様式変更

--- src/main/java/com/example/ninthhomework/domain/user/service/CharactersServiceImpl.java
+++ src/main/java/com/example/ninthhomework/domain/user/service/CharactersServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CharactersServiceImpl implements CharactersService {
@@ -18,10 +19,10 @@ public class CharactersServiceImpl implements CharactersService {
     }
 
     public List<Characters> findByAge(Integer age) {
-        if (age != null) {
-            return charactersMapper.searchByAge(age);
-        } else {
+        if (Objects.isNull(age)) {
             return charactersMapper.findAll();
+        } else {
+            return charactersMapper.searchByAge(age);
         }
     }
 }
